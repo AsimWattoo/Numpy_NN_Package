16f5c04c99518c5ef4dd322bd72d6e507ab89831
AsimWattoo <asimwattoo6@gmail.com>
2023-08-11 10:38:48 +0500

    Changed the package name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='numpy_nn',
-    version='0.1.0',    
+    version='0.1.1',    
     description='A custom neural network implementation',
     url='https://github.com/AsimWattoo/Numpy_NN_Package.git',
     author='Muhammad Asim',
     author_email='asimwattoo6@gmail.com',
     license='BSD 2-clause',
-    packages=['Numpy NN'],
+    packages=['numpy_nn'],
     install_requires=['pandas',
                       'numpy',
                       'matplotlib'                     
